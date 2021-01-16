6d817ac971e8e48d04a925b501aa1e10ec4369d8
johncoryk <coryk0789@gmail.com>
2021-01-16 17:33:48 -0500

    code cleanup

--- src/pages/recipes/[id].js
+++ src/pages/recipes/[id].js
@@ -4,7 +4,7 @@ export default function Recipe() {
   return (
     <>
       <h1>Hello</h1>
-      <h1>this is just testing</h1>
+      <h1>this is just testing a dyanmic route</h1>
     </>
   );
 }
