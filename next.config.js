09570a5720ea74ddbd781a00090f3541ec7ccbaf
johncoryk <coryk0789@gmail.com>
2021-01-17 22:38:45 -0500

    cleaned up code

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	env: {
-		customApiKey: '7733df48bd0345e7a65a629545c5a4d5',
-	},
-};
--- src/pages/recipes/[name].js
+++ src/pages/recipes/[name].js
@@ -3,13 +3,13 @@ import styled from '@emotion/styled';
 
 export default function Recipe({ recipeData }) {
 	const router = useRouter();
-	const { id } = router.query;
+	const { name } = router.query;
 
 	return (
 		<>
 			<h1>Hello</h1>
 			<h1>this is just testing a dyanmic route</h1>
-			<i>{id}</i>
+			<i>{name}</i>
 		</>
 	);
 }
